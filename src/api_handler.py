543b453945bd7897bbadc995a52b6bd28ce27e2c
«tashka-zu» <zubkovanatska21@gmail.com>
2025-06-26 21:18:01 +0300

    add new code to src files

--- src/api_handler.py
+++ src/api_handler.py
@@ -1,39 +1,24 @@
 import requests
 
-def get_employers(employer_ids):
+def search_employers(keyword):
     """
-    Получает данные о работодателях по их идентификаторам.
+    Поиск работодателей по ключевому слову
+    """
+    url = f"https://api.hh.ru/employers"
+    params = {'text': keyword}
+    response = requests.get(url, params=params)
+    employer_ids = []
 
-    :param employer_ids: Список идентификаторов работодателей.
-    :return: Список данных о работодателях.
-    """
-    employers = []
-    for employer_id in employer_ids:
-        url = f"https://api.hh.ru/employers/{employer_id}"
-        response = requests.get(url)
-        if response.status_code == 200:
-            employers.append(response.json())
-    return employers
-
-def get_vacancies(employer_id):
-    """
-    Получает данные о вакансиях для конкретного работодателя.
-
-    :param employer_id: Идентификатор работодателя.
-    :return: Список данных о вакансиях.
-    """
-    url = f"https://api.hh.ru/vacancies?employer_id={employer_id}"
-    response = requests.get(url)
     if response.status_code == 200:
-        return response.json()['items']
-    return []
+        employers = response.json()['items']
+        for employer in employers:
+            employer_ids.append(employer['id'])
+            print(f"Company: {employer['name']}, ID: {employer['id']}")
+
+    return employer_ids
 
 # Пример использования
 if __name__ == "__main__":
-    # Пример списка идентификаторов работодателей
-    example_employer_ids = [123, 456, 789]  # Замените на реальные идентификаторы
-    employers = get_employers(example_employer_ids)
-    print(employers)
-
-    vacancies = get_vacancies(example_employer_ids[0])
-    print(vacancies)
+    keyword = "Yandex"
+    employer_ids = search_employers(keyword)
+    print(employer_ids)
